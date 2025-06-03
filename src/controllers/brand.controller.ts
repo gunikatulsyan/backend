5c77f0ba553c94bddce69fb6b6266151c4e4513e
Gunika Tulsyan <tulsyangunika@gmail.com>
2025-06-03 13:34:56 +0545

    validated brands and products

--- src/controllers/brand.controller.ts
+++ src/controllers/brand.controller.ts
@@ -1,7 +1,9 @@
 import { Request } from "express"
 import { pool } from "../config/db"
+import Joi from "joi";
 
 export const getAllBrands = async(req:any,res:any) => {
+    //pagination and filter
     const {name,id,is_active,limit,page} = req.query
     let query = `SELECT * from brands WHERE 1=1`;
     if(name) query = query + ` AND name = "${name}"`
@@ -22,6 +24,13 @@ export const getSingleBrand = async(req:any,res:any) =>{
 };
 export const createBrand = async(req:Request, res:any) =>{
     const { name, description, is_active } = req.body;
+
+    const {error} = brandDataValidation(req.body) //validation
+    if(error) return res.status(400).json({msg: error.details[0].message});
+
+    const [nameExist]:any = await pool.query(`SELECT * from brands where name ="${name}"`);//checking if brand name already exist or not
+    if(nameExist.length) return res.status(400).json({msg:"Brand name already exist"});
+
     const query = ` INSERT INTO brands( name, description, is_active) VALUES (?, ?, ?)`;
     const values = [ name, description, is_active];
     const [result]:any = await pool.query(query,values)
@@ -40,6 +49,12 @@ export const updateBrand = async(req:any, res:any) =>{
 
     const {is_active, name, description} = req.body
 
+    const {error} = brandDataValidation(req.body)//validation
+    if(error) return res.status(400).json({msg: error.details[0].message});
+
+    const [nameExist]:any = await pool.query(`SELECT * from brands where name ="${name}"`);// checking in brand alr exist
+    if(nameExist.length) return res.status(400).json({msg:"Brand name already exist"});
+
     let updatedDataQuery = []
 
     if(is_active) updatedDataQuery.push(`is_active = ${is_active}`);
@@ -51,4 +66,14 @@ export const updateBrand = async(req:any, res:any) =>{
     let query = ` UPDATE brands SET ${updatedDataQuery.join(', ')} WHERE brandid= ${id}`
     const brand = await pool.query(query)
     return res.status(200).json({msg: "Brand updated successfully", brand})
+}
+
+//validation
+const brandDataValidation = (data:any)=>{
+    const schema= Joi.object({
+        name:Joi.string(),
+        description:Joi.string().required(),
+        is_active:Joi.number().integer().max(1).min(0)
+    }).options({allowUnknown: true})
+    return schema.validate(data)
 }
--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,8 +1,10 @@
 import { Request } from "express"
 import { pool } from "../config/db"
+import Joi from "joi"
 
 export const getAllProducts = async(req:any,res:any) => {
-    const {color,limit,page,minPrice,category,id, name, quantity, brand, maxPrice} = req.query
+    //pagination and filter 
+    const {color,limit,page,minPrice,category,id, name, quantity, brand_id, maxPrice} = req.query
     let query = `SELECT * from products WHERE 1=1`;
     if(color) query = query + ` AND color = "${color}"`
     if(minPrice) query = query + ` AND price>= ${minPrice} `
@@ -11,7 +13,7 @@ export const getAllProducts = async(req:any,res:any) => {
     if(id) query = query + ` AND id = ${id}`
     if(name) query = query + ` AND name = "${name}"`
     if(quantity) query = query + ` AND quantity<= ${quantity}`
-    if(brand) query = query + ` AND brand ="${brand}"`
+    if(brand_id) query = query + ` AND brand_id ="${brand_id}"`
     if(limit) query = query + ` LIMIT ${limit}`
     if(page) query = query + ` OFFSET ${(page-1)*limit}`
     const [products] = await pool.query(query)
@@ -27,9 +29,16 @@ export const getSingleProduct = async(req:any, res:any) =>{
 
 export const createProduct = async(req:Request, res:any) =>{
     console.log(req.body);
-    const { name, price, image, color, quantity, category, brand } = req.body;
-    const query = `INSERT INTO PRODUCTS (name, price, image, color, quantity, category, brand) VALUES (?, ?, ?, ?, ?, ?, ?)`;
-    const values = [name, price, image, color, quantity, category, brand];
+    const { name, price, image, color, quantity, category, brand_id } = req.body;
+
+    const {error} =  productDataValidation(req.body)//validation
+    if(error) return res.status(400).json({msg: error.details[0].message})
+
+    const [brandExist]: any = await pool.query(`SELECT * from brands where id = ${brand_id}`)//making sure the product brand alr exists
+    if(!brandExist.length) return res.status(400).json({msg:"Brand not exist"})
+
+    const query = `INSERT INTO PRODUCTS (name, price, image, color, quantity, category, brand_id) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+    const values = [name, price, image, color, quantity, category, brand_id];
     const [result]:any  = await pool.query(query, values)
     return res.status(200).json({msg:" product created successfully",id: result.insertId})
 
@@ -44,16 +53,36 @@ export const deleteProduct = async(req:any, res:any) =>{
 
 export const updateProduct = async(req:any, res:any) =>{
     const{id} = req.query;
-    const {color, category, price, name, brand, quantity} = req.body
+    const {color, category, price, name, brand_id, quantity} = req.body
+
+    const {error} =  productDataValidation(req.body)
+    if(error) return res.status(400).json({msg: error.details[0].message});
+    
+    const [brandExist]: any = await pool.query(`SELECT * from brands where id = ${brand_id}`);
+    if(!brandExist.length) return res.status(400).json({msg:"Brand not exist"});
+
     let updatedProduct = []
     if(color) updatedProduct.push(`color= "${color}"`);
     if(category) updatedProduct.push(`category= "${category}"`);
     if(price) updatedProduct.push(`price= ${price}`);
     if(name) updatedProduct.push(`name= "${name}"`);
-    if(brand) updatedProduct.push(`brand= "${brand}"`);
+    if(brand_id) updatedProduct.push(`brand_id= "${brand_id}"`);
     if(quantity) updatedProduct.push(`quantity= ${quantity}`);
     if(updatedProduct.length === 0) return res.status(200).json({msg:"No data to update"})
     let query = `UPDATE products set ${updatedProduct.join(', ')} WHERE id=${id}`
     const product = await pool.query(query)
     return res.status(200).json({msg: " Product updated successfully", product})
 }
+
+const productDataValidation = (data:any)=>{
+    const schema=Joi.object({
+        name:Joi.string().required(),
+        price:Joi.number().required(),
+        image:Joi.string().required(),
+        color:Joi.string().required(),
+        quantity:Joi.number().required(),
+        category:Joi.string().required(),
+        brand_id:Joi.number().required()
+    }).options({allowUnknown: true})
+    return schema.validate(data)
+} 
